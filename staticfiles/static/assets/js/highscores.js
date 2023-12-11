40696441fa88c271efbb341ca51e01025f538157
Muhammed Ibrahim Amanokhai <ibrahimjunior08@gmail.com>
2023-12-10 16:35:35 -0800

    Update highscores.js

--- staticfiles/static/assets/js/highscores.js
+++ staticfiles/static/assets/js/highscores.js
@@ -1,8 +1,22 @@
-const highScoresList = document.getElementById("highScoresList");
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const highScoresList = document.getElementById('highScoresList');
+const finalScore = document.getElementById('finalScore');
 
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+finalScore.innerText = "High Scores";
+
+// Display high scores in the list
 highScoresList.innerHTML = highScores
-  .map(score => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>`;
+  .map((score) => {
+    return `<li>${score.name}: ${score.score}</li>`;
   })
-  .join("");
+  .join('');
+
+// Add a button to clear high scores
+highScoresList.innerHTML += `<button onclick="clearHighScores()">Clear High Scores</button>`;
+
+// Function to clear high scores
+clearHighScores = () => {
+  localStorage.removeItem('highScores');
+  highScoresList.innerHTML = '<p>High Scores Cleared</p>';
+};
