2b694ef83b90fc476e6aaf0cc8e3aeb486e3738a
Muhammed Ibrahim Amanokhai <ibrahimjunior08@gmail.com>
2023-12-10 17:23:57 -0800

    Update admin.py

--- scholarpad/admin.py
+++ scholarpad/admin.py
@@ -3,12 +3,12 @@ from .models import Feature
 from .models import UserProfile
 from .models import Quiz
 from .models import UserScore
-from .models import HighScores,
+from .models import HighScore,
 
 # Register your models here.
 admin.site.register(Feature),
 admin.site.register(UserProfile),
 admin.site.register(Quiz),
-admin.site.register(HighScores),
+admin.site.register(HighScore),
 admin.site.register(UserScore)
 
