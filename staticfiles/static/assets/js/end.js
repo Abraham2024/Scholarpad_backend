a79957a22db4410e79f78a9721a249351c1bd81f
Muhammed Ibrahim Amanokhai <ibrahimjunior08@gmail.com>
2023-12-10 16:17:03 -0800

    Update end.js

--- staticfiles/static/assets/js/end.js
+++ staticfiles/static/assets/js/end.js
@@ -1,4 +1,4 @@
-const username = document.getElementById('username');
+const usernameInput = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
@@ -9,22 +9,24 @@ const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
 
-username.addEventListener('keyup', () => {
-    saveScoreBtn.disabled = !username.value;
+usernameInput.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !usernameInput.value;
 });
 
 saveHighScore = (e) => {
     e.preventDefault();
-    alert("congratulations you are a scholar");
+    alert("Congratulations! You are a scholar");
 
+    const user = usernameInput.value; // Update to use the username input value
     const score = {
         score: mostRecentScore,
-        name: username.value,
+        name: user,
     };
+
     highScores.push(score);
     highScores.sort((a, b) => b.score - a.score);
-    highScores.splice(5);
+    highScores.splice(MAX_HIGH_SCORES);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('highscores.html');
+    window.location.assign('high_scores');
 };
