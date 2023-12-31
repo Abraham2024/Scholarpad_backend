dd239ddac2576bb0fff9bf555adc2a024e1d072b
Abraham2024 <ibrahimjunior08@gmail.com>
2023-12-31 09:58:53 +0100

    my_commit

--- scholar/settings.py
+++ scholar/settings.py
@@ -123,8 +123,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "scholarpad.info.gmail.com"
-EMAIL_HOST_PASSWORD = "qlafbjuoieuqwsqt"
+EMAIL_HOST_USER = "scholarpad.info@gmail.com"
+EMAIL_HOST_PASSWORD = "rgvgocciubfaohpt"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
