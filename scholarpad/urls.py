4bcc74b82f85a9f5d1341dc8edd3cd83252a728e
Abraham2024 <ibrahimjunior08@gmail.com>
2023-12-31 12:07:59 +0100

    my_commit

--- scholarpad/urls.py
+++ scholarpad/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings 
 from django.contrib import admin 
 from django.urls import path, re_path
-
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -12,6 +12,10 @@ urlpatterns = [
          path("login", views.login, name = "login"),
          path("signup",views.signup, name = "signup"),
          path("logout", views.logout, name = "logout"),
+         path("reset_password/", auth_views.PasswordResetView.as_view(), name = "reset_password"),
+         path("reset_password_sent/", auth_views.PasswordResetDoneView.as_view(), name = "password_reset_done"),
+         path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name = 'password_reset_confirm'), 
+         path("reset_password_complete/", auth_views.PasswordResetCompleteView.as_view(), name = "password_reset_complete"),
          path("games", views.games, name = "games"),
          path('combo', views.combo, name = "combo"),
     path("jamb", views.jamb, name = "jamb"),
