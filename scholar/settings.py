d7eecc74671c5bf369b060c6a878f3c69f4b23b0
Muhammed Ibrahim Amanokhai <ibrahimjunior08@gmail.com>
2024-01-02 14:58:43 +0100

    Update settings.py

--- scholar/settings.py
+++ scholar/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&*#^s-!)6ixkm#iza8#*t0m-@!9@b35@e1gzezlv-#huokik&%'
+SECRET_KEY = os.environ["SECRET_KEY"]
+#SECRET_KEY = #'django-insecure-&*#^s-!)6ixkm#iza8#*t0m#-@!9@b35@e1gzezlv-#huokik&%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
